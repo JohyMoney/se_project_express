b4f8095688d7a9e44bc36f8379708692b1d83bfc
JohnnyMoney <wyndrockjohn@gmail.com>
2026-05-27 06:53:46 -0400

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+const UnauthorizedError = require("../errors/unauthorized-error");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validator.isURL,
+      message: "Invalid avatar URL",
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Invalid email",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        throw new UnauthorizedError("Incorrect email or password");
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          throw new UnauthorizedError("Incorrect email or password");
+        }
+
+        return user;
+      });
+    });
+};
+
+module.exports = mongoose.model("user", userSchema);
+
